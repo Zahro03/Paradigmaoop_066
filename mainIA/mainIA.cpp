59459ef7cee91422512a6d40ff4ccfbbcd92ede6
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-05-24 18:03:58 +0700

    menampilkan method

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -10,7 +10,7 @@ int main() {
 	ibu* varIbu2 = new ibu("novi");
 	anak* varAnak1= new anak("tono");
 	anak* varAnak2= new anak("rini");
-	anak* varAnak3 = new anak("Dewi");
+	anak* varAnak3 = new anak("Dewi"); 
 
 	varIbu->tambahAnak(varAnak1);
 	varIbu->tambahAnak(varAnak2);
