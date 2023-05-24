4097854b0ee2e4e0e9bf78b3a1692a53f414343b
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-05-24 13:39:35 +0700

    menampilkan method cetakDokter  pada daftar dokter yang menangani pasien

--- Paradigmaoop/Asosiasi.h
+++ Paradigmaoop/Asosiasi.h
@@ -42,7 +42,7 @@ void pasien::cetakDokter() {
 		cout << a->nama << "\n";
 	}
 	cout << endl;
-}
+} 
 void dokter::tambahPasien(pasien* pPasien){
 	daftar_pasien.push_back(pPasien);
 	pPasien->tambahDokter(this);
